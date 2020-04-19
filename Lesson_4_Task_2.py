f18a4348addb3c24c6b67d238c03800c0c45616f
Dmitry-I-S <dmitry.slinchenko@gmail.com>
2020-04-19 11:43:33 +0300

    Исправление ошибки в Задании 2 Урока 4 - сравнение алгоритмов поиска простых чисел

--- Lesson_4_Task_2.py
+++ Lesson_4_Task_2.py
@@ -29,23 +29,28 @@ def return_simple(x):
 # Метод 2 - с решетом.
 def sieve(x):
     """Возвращает простое число по его номеру из списка простых чисел"""
-    len_ = x * x                    # прогнозиремый размер массива - подсказка найдена в википедии
+    len_ = x * x + 2    # прогнозиремый размер массива, прибавляем 2 для содания массива в случае х = 2 ("базовый" случай для первого простого числа)
     arr = [k for k in range(len_)]
     arr[1] = 0
     simple_counter = 1
     for i in range(2, len_):
         if arr[i] != 0:
-            if simple_counter == x:
+            if simple_counter == x:     # если число не проколото - оно простое, увеличиваем счетчик
                 return arr[i]
             simple_counter += 1
-            j = i + i
+            j = i ** 2                  # улучшение алгоритма, сокращение длины прохода - т.к. до i**2 все составные числа будут проколоты
             while j < len_:
                 arr[j] = 0
                 j += i
 
-# Тестирование алгоритмов:
-# for i in range(2, 100):
-#     print(i, '---', return_simplsieve(i))
+
+# Тест-сравнение функций
+for i in range(1, 100):
+    if sieve_2(i) != sieve(i):
+        print('Критическая ошибка')
+else:
+    print('sieve_2(i) = sieve(i)')
+
 
 # Тестирование первого способа:
 print(timeit.timeit('return_simple(50)', number=50, globals=globals()))  ## 0.04457890800000001
@@ -66,13 +71,13 @@ cProfile.run('return_simple(800)')  # 6132    0.343    0.000    0.343    0.000 L
 # Вывод 1 - Зависимость, вероятно, параболическая - два вложенных цикла - в цикл while вложен цикл for. Согласно cProfile программа буксует
 # на функции  is_simple, которая определяет, яв-ся ли число простым. Это вполне ожидаемо...
 
-# Тестирование второго способа с решетом:
-print(timeit.timeit('sieve(50)', number=50, globals=globals()))  ## 0.07131949700000018
-print(timeit.timeit('sieve(100)', number=50, globals=globals()))  ## 0.30583471000000007
-print(timeit.timeit('sieve(150)', number=50, globals=globals()))  ## 0.7055815579999996
-print(timeit.timeit('sieve(200)', number=50, globals=globals()))  ## 1.2900327349999996
-print(timeit.timeit('sieve(400)', number=50, globals=globals()))  ## 5.976309650999999
-print(timeit.timeit('sieve(800)', number=50, globals=globals()))  ## 26.050270579000003
+# Тестирование второго способа с решетом:                         Прокалываем с j = i * 2    Прокалываем с j = i **2
+print(timeit.timeit('sieve(50)', number=50, globals=globals()))  ## 0.07131949700000018     ##0.06006963500000495
+print(timeit.timeit('sieve(100)', number=50, globals=globals()))  ## 0.30583471000000007    ##0.26010760900000207
+print(timeit.timeit('sieve(150)', number=50, globals=globals()))  ## 0.7055815579999996    ##0.6135311479999999
+print(timeit.timeit('sieve(200)', number=50, globals=globals()))  ## 1.2900327349999996    ##1.1108276900000007
+print(timeit.timeit('sieve(400)', number=50, globals=globals()))  ## 5.976309650999999    ##5.096983688999998
+print(timeit.timeit('sieve(800)', number=50, globals=globals()))  ## 26.050270579000003    ##22.519397166000005
 
 cProfile.run('sieve(50)')   # 0.000    0.000    0.002    0.002 {built-in method builtins.exec}
 cProfile.run('sieve(100)')  # 0.000    0.000    0.006    0.006 {built-in method builtins.exec}
@@ -84,3 +89,6 @@ cProfile.run('sieve(800)')  # 0.000    0.000    0.556    0.556 {built-in method 
 # Вывод 2 - Зависимость, вероятно, параболическая - так же, как и в первом случае (графики подтверждают)
 # - ее могут давать два воженных цикла. Но в целом, этот алгоритм работает медленнее,
 # видимо, из-за создания списка и обращения к нему.
+# ************************************************
+# Обновление - При замене старта прокалывания с j=i*2 на j=i**2 мы сокращаем пробег по каждому циклу.
+# И это отражаемтся на времени выполнения - оно уменьшилось процентов на 15%!
